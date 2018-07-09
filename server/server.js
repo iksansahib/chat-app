a0cdb314df59ee763d7c68aa163a6d4644e85613
Iksan Maricar <iksansahib@gmail.com>
2018-07-09 23:35:20 +0700

    Emit broadcasting message

--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user');
+
+    socket.on('sendChat', (chat) => {
+        io.emit('receiveChat', {
+            from: chat.from,
+            message: chat.message
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Disconnected');
+    });
+});
+server.listen(port, () => {
     console.log('Express Running');
 });
